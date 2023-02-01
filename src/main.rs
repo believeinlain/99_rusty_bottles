3840d085e296bc3a715e870c051f29cb116da443
Stephanie Aelmore <steph.aelmore@gmail.com>
2023-02-01 10:44:38 -1000

    added possible direction for generics

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,25 @@ mod bottles {
 
     use Bottles::*;
 
+    // trait QuacksLikeANumber {} // Optionally add some type bounds here also
+
+    // // Implement this trait for everything that also implements the following
+    // impl<T> QuacksLikeANumber for T where
+    //     T: Add<Output = Self>
+    //         + Div<Output = Self>
+    //         + Mul<Output = Self>
+    //         + Sub<Output = Self>
+    //         + Rem<Output = Self>
+    //         + Copy
+    //         + PartialEq
+    //         + PartialOrd
+    //         // Add the below if you don't want floats.
+    //         + Eq
+    //         + Ord
+            
+    // {
+    // }
+
     impl<T> Bottles<T> where T: Into<usize> + From<u8> + Sub<u8, Output = T> + Display + Copy {
         pub fn new(num: T) -> Self {
             match Into::<usize>::into(num) {
